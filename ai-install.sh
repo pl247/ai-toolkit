f4b51a2030d310d7baeb908da15fc120dbad17ea
plemaist <plemaist@cisco.com>
2023-06-26 09:25:49 -0500

    removed CUDA 11.7 and 20.04 items

--- ai-install.sh
+++ ai-install.sh
@@ -10,28 +10,9 @@ sudo mkdir /ai/software
 # Get NVIDIA GPU Drivers as well as CUDA
 echo -e "\n==================Get NVIDIA GPU Drivers as well as CUDA=================="
 sleep 3
-# 12.1 that does not work
+# 12.1 for 22.04 (more testing needed)
 #sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb -P /ai/software
 #sudo dpkg -i /ai/software/cuda-keyring_1.0-1_all.deb
-#sudo apt-get update
-#sudo apt-get -y install cuda
-
-# 11.7 for 20.04
-#sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin -P /ai/software
-#sudo mv /ai/software/cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#sudo wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb -P /ai/software
-#sudo dpkg -i /ai/software/cuda-repo-ubuntu2004-11-7-local_11.7.0-515.43.04-1_amd64.deb
-#sudo cp /var/cuda-repo-ubuntu2004-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
-#sudo apt-get update
-#sudo apt-get install cuda-11-7
-# to remove try sudo apt purge nvidia-*
-
-# 11.7 for 22.04
-#sudo wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin -P /ai/software
-#sudo mv /ai/software/cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
-#sudo wget https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb -P /ai/software
-#sudo dpkg -i /ai/software/cuda-repo-ubuntu2204-11-7-local_11.7.0-515.43.04-1_amd64.deb
-#sudo cp /var/cuda-repo-ubuntu2204-11-7-local/cuda-*-keyring.gpg /usr/share/keyrings/
 #sudo apt-get update
 #sudo apt-get -y install cuda
 
@@ -90,7 +71,7 @@ pip install -r /home/ubuntu/text-generation-webui/requirements.txt
 # Install first LLM model
 echo -e "\n==================Installing LLM Models=================================="
 cd /home/ubuntu/text-generation-webui
-#python3 download-model.py facebook/opt-350m
+python3 download-model.py facebook/opt-350m
 #python3 download-model.py TheBloke/vicuna-7B-1.1-HF
 #python3 download-model.py TheBloke/vicuna-13B-1.1-HF
 #python3 download-model.py TheBloke/Wizard-Vicuna-30B-Uncensored-fp16
